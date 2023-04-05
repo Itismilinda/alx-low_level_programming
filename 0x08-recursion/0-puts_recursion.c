bda3afc33483b99e0739b9b862ccbc66332a2f60
Itismilinda <patience.daniel.kindai@gmail.com>
2023-04-05 08:02:20 -0700

    0-puts_recursion.c updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,16 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- *
- * Return: void
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        putchar(*s);
-        _puts_recursion(s+1);
-    }
-    else
-	    putchar("\n");
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
