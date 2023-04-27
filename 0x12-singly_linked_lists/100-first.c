e662fffcc8c9a3dd70bbeda372b3b094817d8e14
patience Daniel <patience.daniel.kindai@gmail.com>
2023-04-27 09:04:38 -0700

    Update to D3

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+void first(void) __attribute__ ((constructor));
 
 /**
  * first - prints a sentence before the main
  * function is executed
  */
+
 void first(void)
 
-}
-
-void first (void)  __attribute__((constructor)) before_main()
 {
-    printf("You're beat! and yet, you must allow,\n"
-           "I bore my house upon my back!\n");
+
+
+    printf  ("You're beat! and yet, you must allow,\n");
+    printf  ("I bore my house upon my back!\n");
+
 }
